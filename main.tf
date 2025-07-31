77e7c0892d57e82130f961d00ba75ce967c80f1e
TEK Student <TEKstudent@vlab.lan>
2025-07-31 11:29:17 -0700

    updated security group

--- main.tf
+++ main.tf
@@ -63,15 +63,15 @@ module "lb_security_group" {
   description = "Security group for load balancer with HTTP ports open within VPC"
   vpc_id      = module.vpc.vpc_id
 
-  ingress_cidr_blocks = ["0.0.0.0/0"]
+  ingress_cidr_blocks = ["10.0.0.0/16"]
   ingress_rules       = ["ssh-tcp"]
   ingress_with_cidr_blocks = [
     {
       from_port   = 22
       to_port     = 22
       protocol    = "tcp"
-      description = "SSH open to the world"
-      cidr_blocks = "0.0.0.0/0"
+      description = "SSH restricted to internal network"
+      cidr_blocks = "10.0.0.0/16"
     }
   ]
 
@@ -84,7 +84,7 @@ module "lb_security_group" {
 resource "aws_ebs_volume" "unencrypted" {
   availability_zone = "us-west-1a"
   size              = 8
-  encrypted         = false # Intentional violation: unencrypted EBS volume
+  encrypted         = true # Remediated: enable encryption
 }
 
 resource "random_string" "lb_id" {
